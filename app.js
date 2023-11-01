88fe40fed26ae2c61e1b2ab39893c3c37ac94a2d
soyounRoh <deresa0223@gmail.com>
2023-11-01 17:08:17 +0900

    Add: 회원가입 라우터,app.js와 연결

--- app.js
+++ app.js
@@ -9,12 +9,17 @@ mongoose.connect('mongodb+srv://WebProjectDB:45654654@alicewebprojectteam8.pqahg
 
 // view 엔진을 ejs를 쓰겠다는 설정
 app.set("view engine", "ejs");
+app.use(express.json({ extended: false }));
 
 // 페이지 로딩 함수
 app.get("/", function (req, res) {
     //console.log(res);
     res.render("index", {}); // views 폴더 밑에 있는 파일을 참조함
 });
+
+// 회원가입 페이지 router 이동
+app.use('/sign-up', require('./routes/sign-up'));
+
 
 // 서버 띄울때 포트 정보 셋팅 및 처음 실행 시 필요한 기능 수행 가능
 app.listen(3000, function () {
---
+++ routes/sign-up.js
@@ -0,0 +1,45 @@
+const { Router } = require('express');
+const { User } = require('../models'); // user model
+const hashPassword = require('../utils/hash-password');
+
+const router = Router();
+
+
+// sign-up
+router.post('/auth/sign-up', async (req, res) => {
+    const { email, password, username, phoneNumber, address, addressDetail } = req.body;
+    
+    // email이 존재하는지 확인
+    try{
+        let user = await User.findOne({ email });
+        // user가 존재하면 반환
+        if(user){
+            return res
+                    .status(409)
+                    .json({ error: "이미 가입된 email 입니다."});
+        }
+
+        // user가 없다면 user에 다음을 생성
+        user = await User.create({
+            email,
+            password : hashPedPassword,
+            username,
+            phoneNumber,
+            address,
+            addressDetail,
+        });
+       
+        // Password 암호화
+        const hashPedPassword = hashPassword(password);
+        
+        // 새로운 user를 db에 저장
+        await user.save();
+        res.redirect('/');
+
+    }catch(e){
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+module.exports = router;
