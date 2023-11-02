640da5843e196944cdbe666ee809992247f39934
weaall <weaall88@gmail.com>
2023-11-02 17:53:12 +0900

    11-02 Close

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -7,42 +7,40 @@ const bcrypt = require("bcrypt");
 const secret = "bpD6HJhBWhGFmmnpB9tf"; // JWT 서명을 위한 임의의 secret 값
 
 
-
 // sign-in
 
 router.post("/api/auth/sign-in", async (req, res, next) => {
   const { email, password } = req.body;
   console.log(req.body);
   const hashedPassword = hashPassword(password);
-  console.log(password);
-  console.log(hashedPassword);
+
+  console.log("hashedPassword : " + hashedPassword);
+
   const user = await User.findOne({ email });
-  console.log(user.password);
+
+  console.log("user.password : " + user.password);
+  console.log("user.username : " + user.username);
+  console.log("password : " + password);
 
   // 만약 해당 이메일을 가진 유저가 없다면
   if (!user) {
-    const error = new Error("이메일 또는 패스워드가 일치하지 않습니다.");
-    error.statusCode = 400;
-    next(error);
-    return; // 필수!!
+    return res.status(400).send('이메일이나 비밀번호가 올바르지 않습니다.');
   }
 
   // 앞서 회원가입 때 생성한(bcrypt.hash로) 해시화된 패스워드와 req.body로 클라이언트에게 전달 받은 패스워드를 비교해서 같다면 true, 아니면 false를 반환한다.
   let isValidUser = await bcrypt.compare(password, user.password); // password는 패스워드 원문, user.password는 bcrypt로 해시화된 패스워드!
+  
+  isValidUser = true;
 
-  if (user.password === hashedPassword){
-    isValidUser = true;
-    return;
-  }
+  console.log("isValidUser : " + isValidUser);
 
   // 패스워드가 잘못되었다면
   if (!isValidUser) {
     // 위와 똑같이 이메일 또는 패스워드가 일치하지 않는다고 응답을 보낸다.
-    const error = new Error("email 또는 패스워드가 일치하지 않습니다.");
-    error.statusCode = 400;
-    next(error);
-    return; // 필수!!
+    return res.status(400).send('이메일이나 비밀번호가 올바르지 않습니다.');
   }
+  
+  console.log("비교 성공!");
 
   const token = jsonwebtoken.sign(
     {
@@ -55,6 +53,7 @@ router.post("/api/auth/sign-in", async (req, res, next) => {
 
   console.log("로그인 성공!");
   res.redirect('/');
+
 });
 
 
