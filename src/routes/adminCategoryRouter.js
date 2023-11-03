ba9a22e298a25e999ffb68471fa6f067864d4ea3
weaall <weaall88@gmail.com>
2023-11-03 17:43:38 +0900

    Feat: delete

--- src/routes/adminCategoryRouter.js
+++ src/routes/adminCategoryRouter.js
@@ -12,7 +12,6 @@ router.get('/api/admin/cartegories', async(req, res) => {
 
 
 // 카테고리 만들기
-
 router.post("/api/admin/cartegories", async (req, res) => {
   const { name } = req.body;
   console.log(name)
@@ -26,6 +25,19 @@ router.post("/api/admin/cartegories", async (req, res) => {
   category = await Category.create({
     name: name,
   });
+});
+
+// 카테고리 수정
+router.delete("/api/admin/cartegories", async (req, res) => {
+  const { name } = req.body;
+ 
+  const category = await Category.deleteOne({ name });
+
+  if (category) {
+    return res.status(404).json({ error: "존재하지 않는 카테고리입니다." });
+  }
+
+  res.status(204).json({ message: category.name + "카테고리를 성공적으로 제거 했습니다."});
 });
 
 module.exports = router;
