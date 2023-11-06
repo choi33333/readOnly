fe1bf67bcc93ba2b7e582281d94cf3698d130661
raramen98 <swtraining98@gmail.com>
2023-11-07 00:29:58 +0900

    add: add js file

---
+++ src/views/category/category.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", async () => {
+    const categories = await fetch("/api/categories") 
+                            .then(result => result.json())
+                            .catch(err => null);
+    
+    if (categories !== null) {
+        const categoryWrapperElem = document.getElementById("category-wrapper");
+        categoryWrapperElem.innerHTML = '';
+
+        for (let i=0; i<categories.length; i++){
+            const categoryElem = document.createElement("a");
+            categoryElem.innerHTML = categories[i].name;
+            categoryElem.setAttribute("href", "?category="+categories[i]._id);   // 다른파일에서 불러오는법
+            categoryWrapperElem.append(categoryElem);
+        }
+    } 
+})
