2f9228de462123176bc732c7cb573177e8cda5cc
weaall <weaall88@gmail.com>
2023-11-07 23:13:26 +0900

    Fix: fix auth

--- src/routes/v1/admin/adminUserRouter.js
+++ src/routes/v1/admin/adminUserRouter.js
@@ -22,21 +22,22 @@ router.get("/", isAdmin, async (req, res, next) => {
 // user 삭제
 router.delete("/:id", isAdmin, async (req, res, next) => {
   const id = req.params.id;
-  const deletedUser = await UserModel.deleteById({ _id: id })
+
+  const deletedUser = await UserModel.deleteOne({ _id: id })
     .lean()
     .catch((error) => {
       error = new Error("사용자가 존재하지 않습니다.");
       error.status = 404;
       return next(error);
     });
-
+    
   if (!deletedUser || deletedUser.length === 0) {
     const error = new Error("사용자가 존재하지 않습니다.");
     error.status = 404;
     return next(error);
   }
 
-  res.status(204).json({
+  res.json({
     error: null,
     data: deletedUser,
     message: "사용자가 삭제되었습니다.",
