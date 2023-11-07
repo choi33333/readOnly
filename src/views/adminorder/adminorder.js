cb358376896ef81ec3c4302748ca47b06ec36866
[SW7] 최재혁 <wogur9817@gmail.com>
2023-11-08 02:52:33 +0900

    add adminorder,ordercheck

--- src/views/adminorder/adminorder.js
+++ src/views/adminorder/adminorder.js
@@ -3,27 +3,34 @@ document.addEventListener("DOMContentLoaded", function() {
     const orderNumberInput = document.getElementById("orderNumber");
     const orderResults = document.getElementById("orderResults");
 
-    searchButton.addEventListener("click", function() {
+    searchButton.addEventListener("click", async function() { 
         const searchTerm = orderNumberInput.value.trim();
         if (searchTerm === "") {
             return;
         }
 
-        // 주문 조회 기능 구현
-        const orders = [
-            { orderNumber: "001", productName: "상품 1", quantity: 3, amount: "$30.00", deliveryStatus: "배송전"},
-            { orderNumber: "002", productName: "상품 2", quantity: 2, amount: "$20.00", deliveryStatus: "배송완료"},
-            // 추가 주문 정보를 여기에 추가
-        ];
+        try {
+            // API 요청을 보내서 주문 정보를 가져오기
+            const response = await fetch('/api/v1/admin/orders', {
+                method: "GET",
+            });
 
-        const filteredOrders = orders.filter(order => order.orderNumber === searchTerm);
-        displayOrders(filteredOrders);
+            if (!response.ok) {
+                throw new Error('error ${response.status}');
+            }
+
+            const orders = await response.json();
+            const filteredOrders = orders.filter(order => order.orderNumber === searchTerm);
+            displayOrders(filteredOrders);
+        } catch (error) {
+            console.error("Error fetching data: " + error);
+        }
     });
 
     function displayOrders(orders) {
         const tableBody = orderResults;
         tableBody.innerHTML = ""; // 이전 결과 삭제
-
+        // 주문을 찾지 못한 경우 메시지를 표시
         if (orders.length === 0) {
             tableBody.innerHTML = "<tr><td colspan='5'>주문을 찾을 수 없습니다.</td></tr>";
         } else {
@@ -40,4 +47,25 @@ document.addEventListener("DOMContentLoaded", function() {
             });
         }
     }
+    async function deleteOrder(orderNumber) {
+        try {
+            // 서버에 주문 삭제 요청
+            const response = await fetch('/api/v1/admin/orders/', {
+                method: "DELETE",
+            });
+    
+            if (!response.ok) {
+                throw new Error(`주문 삭제 중 오류 발생 (${response.status})`);
+            }
+    
+            // 성공적으로 주문을 삭제한 경우 해당 주문을 제거
+            const orderRow = document.querySelector(`tr[data-order-number="${orderNumber}"]`);
+            if (orderRow) {
+                orderRow.remove();
+            }
+        } catch (error) {
+            console.error("주문 삭제 오류: " + error);
+        }
+    }
+    
 });
