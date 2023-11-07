b8f10f13042e8a3c69b646f26589fa18f6d9a5d5
weaall <weaall88@gmail.com>
2023-11-07 16:08:18 +0900

    Feat: error handler

--- src/app.js
+++ src/app.js
@@ -38,14 +38,13 @@ app.use((req, res, next) => {
 });
 
 // 에러 핸들러 등록
-// app.use((error, req, res, next) => {
-//   console.log(error);
-//   res.statusCode = error.statusCode ?? 500;
-//   res.json({
-//     data: null,
-//     error: error.message,
-//   });
-// });
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).json({
+    error: "Internal server error",
+    data: null,
+  });
+});
 
 // 서버 띄울때 포트 정보 셋팅 및 처음 실행 시 필요한 기능 수행 가능
 app.listen(3000, function () {
