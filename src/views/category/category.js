2357e642aac6f0aa171ccc291ad01faaae051745
raramen98 <swtraining98@gmail.com>
2023-11-07 14:42:16 +0900

    add: add dynamic html

--- src/views/category/category.js
+++ src/views/category/category.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", async () => {
+    // 카테고리 데이터 불러오기
     const categories = await fetch("/api/categories") 
                             .then(result => result.json())
                             .catch(err => null);
@@ -6,8 +7,10 @@ window.addEventListener("load", async () => {
     if (categories !== null) {
         // console.log(categories);
 
+        // 카테고리 sidebar
         const categoryWrapperElem = document.getElementById("category-wrapper");
         categoryWrapperElem.innerHTML = '';
+
 
         for (let i=0; i<categories.data.length; i++){
             const categoryElem = document.createElement("a");
@@ -15,5 +18,61 @@ window.addEventListener("load", async () => {
             categoryElem.setAttribute("href", "?category="+categories.data[i]._id);   
             categoryWrapperElem.append(categoryElem);
         }
+
+        // 카테고리 body
+        const bestsellerTextElem = document.getElementById("bestseller-title");
+        bestsellerTextElem.innerHTML = "";
+
+        const newbookTextElem = document.getElementById("newbook-title");
+        newbookTextElem.innerHTML = "";
+
+        const viewallTextElem = document.getElementById("view-all");
+        viewallTextElem.innerHTML = "";
+
+        const bestsellerElem = document.createElement("h2");
+        const newbookElem = document.createElement("h2");
+        const viewallElem = document.createElement("h2");
+
+        // localhost:3000/category/index.html?category=6548d90cf5bf2ae69dafd923
+        const categorize = location.href.split("category=")[1];
+       
+        if (categorize === "6548d90cf5bf2ae69dafd923"){
+            bestsellerElem.innerHTML = categories.data[0].name + " 베스트셀러";
+            bestsellerTextElem.append(bestsellerElem);
+
+            newbookElem.innerHTML = categories.data[0].name + " 신간";
+            newbookTextElem.append(newbookElem);
+
+            viewallElem.innerHTML = categories.data[0].name + " 전체보기";
+            viewallTextElem.append(viewallElem);
+            
+        }
+
+        if (categorize === "6548db0af4083cc0993650a3"){
+            bestsellerElem.innerHTML = categories.data[1].name + " 베스트셀러";
+            bestsellerTextElem.append(bestsellerElem);
+
+            newbookElem.innerHTML = categories.data[1].name + " 신간";
+            newbookTextElem.append(newbookElem);
+
+            viewallElem.innerHTML = categories.data[1].name + " 전체보기";
+            viewallTextElem.append(viewallElem);
+            
+        }
     } 
+
+
+    // 도서 정보 불러오기
+    const products = await fetch("/api/products") 
+                            .then(result => result.json())
+                            .catch(err => null);
+    
+    if (products !== null) {
+        console.log(products);
+        const nameElem = document.getElementById("name");
+        
+    }
 })
+
+
+
