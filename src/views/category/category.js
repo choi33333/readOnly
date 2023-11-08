1b2a1c58b27c27dddcfe000caa4117e543905c53
raramen98 <swtraining98@gmail.com>
2023-11-09 00:43:41 +0900

    임시저장2

--- src/views/category/category.js
+++ src/views/category/category.js
@@ -1,98 +1,104 @@
 window.addEventListener("load", async () => {
-	// 카테고리 데이터 불러오기
-	const categories = await fetch("/api/v1/categories")
-		.then((result) => result.json())
-		.catch((err) => null);
+    // 카테고리 데이터 불러오기
+    const categories = await fetch("/api/v1/categories") 
+                            .then(result => result.json())
+                            .catch(err => null);
+    
+    if (categories !== null) {
+        // console.log(categories);
 
-	if (categories !== null) {
-		// console.log(categories);
+        // 카테고리 sidebar
+        const categoryWrapperElem = document.getElementById("category-wrapper");
+        categoryWrapperElem.innerHTML = '';
 
-		// 카테고리 sidebar
-		const categoryWrapperElem = document.getElementById("category-wrapper");
-		categoryWrapperElem.innerHTML = "";
 
-		for (let i = 0; i < categories.data.length; i++) {
-			const categoryElem = document.createElement("a");
-			categoryElem.innerHTML = categories.data[i].name;
-			categoryElem.setAttribute("href", "?category=" + categories.data[i]._id);
-			categoryWrapperElem.append(categoryElem);
-		}
+        for (let i=0; i<categories.data.length; i++){
+            const categoryElem = document.createElement("a");
+            categoryElem.innerHTML = categories.data[i].name;
+            categoryElem.setAttribute("href", "?category="+categories.data[i]._id);   
+            categoryWrapperElem.append(categoryElem);
+        }
 
-		// 카테고리 body
-		const bestsellerTextElem = document.getElementById("bestseller-header");
-		bestsellerTextElem.innerHTML = "";
 
-		const newbookTextElem = document.getElementById("newbook-header");
-		newbookTextElem.innerHTML = "";
+        // 카테고리 body
+        const bestsellerTextElem = document.getElementById("bestseller-header");
+        bestsellerTextElem.innerHTML = "";
 
-		const viewallTextElem = document.getElementById("view-all-header");
-		viewallTextElem.innerHTML = "";
+        const newbookTextElem = document.getElementById("newbook-header");
+        newbookTextElem.innerHTML = "";
 
-		const bestsellerElem = document.createElement("h2");
-		const newbookElem = document.createElement("h2");
-		const viewallElem = document.createElement("h2");
+        const viewallTextElem = document.getElementById("view-all-header");
+        viewallTextElem.innerHTML = "";
 
-		// localhost:3000/category/index.html?category=6548d90cf5bf2ae69dafd923
-		const categorize = location.href.split("category=")[1];
+        const bestsellerElem = document.createElement("h2");
+        const newbookElem = document.createElement("h2");
+        const viewallElem = document.createElement("h2");
 
-		if (categorize === "6548d90cf5bf2ae69dafd923") {
-			bestsellerElem.innerHTML = categories.data[0].name + " 베스트셀러";
-			bestsellerTextElem.append(bestsellerElem);
+        // localhost:3000/category/index.html?category=6548d90cf5bf2ae69dafd923
+        const categorize = location.href.split("category=")[1];
+       
+        if (categorize === "6548d90cf5bf2ae69dafd923"){
+            bestsellerElem.innerHTML = categories.data[0].name + " 베스트셀러";
+            bestsellerTextElem.append(bestsellerElem);
 
-			newbookElem.innerHTML = categories.data[0].name + " 신간";
-			newbookTextElem.append(newbookElem);
+            newbookElem.innerHTML = categories.data[0].name + " 신간";
+            newbookTextElem.append(newbookElem);
 
-			viewallElem.innerHTML = categories.data[0].name + " 전체보기";
-			viewallTextElem.append(viewallElem);
-		}
+            viewallElem.innerHTML = categories.data[0].name + " 전체보기";
+            viewallTextElem.append(viewallElem);
+            
+        }
 
-		if (categorize === "6548db0af4083cc0993650a3") {
-			bestsellerElem.innerHTML = categories.data[1].name + " 베스트셀러";
-			bestsellerTextElem.append(bestsellerElem);
+        if (categorize === "6548db0af4083cc0993650a3"){
+            bestsellerElem.innerHTML = categories.data[1].name + " 베스트셀러";
+            bestsellerTextElem.append(bestsellerElem);
 
-			newbookElem.innerHTML = categories.data[1].name + " 신간";
-			newbookTextElem.append(newbookElem);
+            newbookElem.innerHTML = categories.data[1].name + " 신간";
+            newbookTextElem.append(newbookElem);
 
-			viewallElem.innerHTML = categories.data[1].name + " 전체보기";
-			viewallTextElem.append(viewallElem);
-		}
-	}
+            viewallElem.innerHTML = categories.data[1].name + " 전체보기";
+            viewallTextElem.append(viewallElem);
+            
+        }
+    } 
 
-	// 도서 정보 불러오기
-	const products = await fetch("/api/v1/products")
-		.then((result) => result.json())
-		.catch((err) => null);
 
-	console.log(products.data);
+    // 도서 정보 불러오기
+    const products = await fetch("/api/v1/products") 
+                            .then(result => result.json())
+                            .catch(err => null);
 
-	// if (products.error || !Array.isArray(products.data.category)) {
-	//     return;
-	// }
+    console.log(products.data);
+    
 
-	const categorize = location.href.split("category=")[1];
-	//현재 주소의 카테고리 값
+    // if (products.error || !Array.isArray(products.data.category)) {
+    //     return;
+    // }
 
-	const filteredProducts = products.data.filter(
-		(product) => product.category === categorize
-	);
-	//현재 주소의 카테고리와 같은 카테고리의 상품 필터
+    const categorize = location.href.split("category=")[1]; 
+    //현재 주소의 카테고리 값
 
-	//베스트셀러 정보 불러오기
-	const bestsellerInfoElem = document.getElementById("bestseller-info");
+    const filteredProducts = products.data.filter(product => product.category === categorize);
+    //현재 주소의 카테고리와 같은 카테고리의 상품 필터
 
-	const copyFilteredProducts1 = [...filteredProducts];
 
-	copyFilteredProducts1.sort((a, b) => b.soldAmount - a.soldAmount);
+    //베스트셀러 정보 불러오기
+    const bestsellerInfoElem = document.getElementById("bestseller-info");
 
-	if (copyFilteredProducts1 !== null) {
-		for (let i = 0; i < 2; i++) {
-			const title = copyFilteredProducts1[i].name;
-			const author = copyFilteredProducts1[i].author;
-			const price = copyFilteredProducts1[i].price;
-			const productInfo = copyFilteredProducts1[i].productInfo;
+    const copyFilteredProducts1 = [...filteredProducts];
 
-			const template = `
-                    <a href=../select-item/?id=${copyFilteredProducts1[i]._id}>
+    copyFilteredProducts1.sort((a,b) => b.soldAmount - a.soldAmount);
+    
+    if (copyFilteredProducts1 !== null) {
+        for (let i=0; i< 2; i++){
+
+            const title = copyFilteredProducts1[i].name;
+            const author = copyFilteredProducts1[i].author;
+            const price = copyFilteredProducts1[i].price;
+            const productInfo = copyFilteredProducts1[i].productInfo;
+
+            const template = `
+                <a href="../select-item/index.html">
                     <img src="../img/bestseller-novel1.jpeg">
                     <h5>${title}</h5>
                     <h6>${author}</h6>
@@ -100,25 +106,27 @@ window.addEventListener("load", async () => {
                     <p>${productInfo}</p>
                 </a>
             `;
-			bestsellerInfoElem.innerHTML += template;
-		}
-	}
+            bestsellerInfoElem.innerHTML += template;
+        }
+    }
 
-	//신간 정보 불러오기
-	const newbookInfoElem = document.getElementById("newbook-info");
+    //신간 정보 불러오기
+    const newbookInfoElem = document.getElementById("newbook-info");
 
-	const copyFilteredProducts2 = [...filteredProducts];
-	console.log(copyFilteredProducts2);
-	copyFilteredProducts2.sort((a, b) => b.releasedDate - a.releasedDate);
+    const copyFilteredProducts2 = [...filteredProducts];
 
-	if (copyFilteredProducts2 !== null) {
-		for (let i = 0; i < copyFilteredProducts2.length; i++) {
-			const title = copyFilteredProducts2[i].name;
-			const author = copyFilteredProducts2[i].author;
-			const price = copyFilteredProducts2[i].price;
-			const productInfo = copyFilteredProducts2[i].productInfo;
-			const template = `
-                <a href=../select-item/?id=${copyFilteredProducts2[i]._id}>
+    copyFilteredProducts2.sort((a,b) => b.releasedDate - a.releasedDate);
+
+    if (copyFilteredProducts2 !== null){
+        for (let i=0; i<2; i++) {
+
+            const title = copyFilteredProducts2[i].name;
+            const author = copyFilteredProducts2[i].author;
+            const price = copyFilteredProducts2[i].price;
+            const productInfo = copyFilteredProducts2[i].productInfo;
+
+            const template = `
+                <a href="../select-item/index.html">
                     <img src="../img/bestseller-novel1.jpeg">
                     <h5>${title}</h5>
                     <h6>${author}</h6>
@@ -126,22 +134,25 @@ window.addEventListener("load", async () => {
                     <p>${productInfo}</p>
                 </a>
             `;
-			newbookInfoElem.innerHTML += template;
-		}
-	}
+            newbookInfoElem.innerHTML += template;
 
-	//전체도서 정보 불러오기
-	const viewAllInfoElem = document.getElementById("view-all-info");
+        }
+    }
+    
+    
+    //전체도서 정보 불러오기
+    const viewAllInfoElem = document.getElementById("view-all-info");
 
-	if (filteredProducts !== null) {
-		for (let i = 0; i < filteredProducts.length; i++) {
-			const title = filteredProducts[i].name;
-			const author = filteredProducts[i].author;
-			const price = filteredProducts[i].price;
-			const productInfo = filteredProducts[i].productInfo;
+    if (filteredProducts !== null) {
+        for (let i=0; i<filteredProducts.length; i++){
 
-			const template = `
-                    <a href=../select-item/?id=${filteredProducts[i]._id}>
+            const title = filteredProducts[i].name;
+            const author = filteredProducts[i].author;
+            const price = filteredProducts[i].price;
+            const productInfo = filteredProducts[i].productInfo;
+
+            const template = `
+                <a href="../select-item/index.html">
                     <img src="../img/bestseller-novel1.jpeg">
                     <h5>${title}</h5>
                     <h6>${author}</h6>
@@ -149,7 +160,10 @@ window.addEventListener("load", async () => {
                     <p>${productInfo}</p>
                 </a>
             `;
-			viewAllInfoElem.innerHTML += template;
-		}
-	}
-});
+            viewAllInfoElem.innerHTML += template;
+        }
+    }
+
+})
+
+
