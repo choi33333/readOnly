f518065ea341b62efce56ce6e07a005ee4908b24
soyounRoh <deresa0223@gmail.com>
2023-11-09 00:51:09 +0900

    feat : controller , service ,router 폴더구조 및 연결 완료(order, admin 제외)

--- src/routes/v1/categoryRouter.js
+++ src/routes/v1/categoryRouter.js
@@ -1,34 +1,27 @@
 const { Router } = require("express");
-const { CategoryModel } = require("../../models");
 const { objectIdValidator, validateError } = require("../../middlewares/validators/index");
+const categoryController = require('../../controllers/categoryController');
 const router = Router();
 
 // 카테고리 조회
-router.get("/", async (req, res, next) => {
-  const categories = await CategoryModel.find({}).lean();
+router.get("/", categoryController.getCategories);
 
-  res.json({
-    error: null,
-    data: categories,
-  });
-});
+// router.get("/:id", objectIdValidator, validateError, async (req, res, next) => {
+//   const id = req.params.id;
 
-router.get("/:id", objectIdValidator, validateError, async (req, res, next) => {
-  const id = req.params.id;
-
-  const category = await CategoryModel.findById(id)
-    .lean();
+//   const category = await CategoryModel.findById(id)
+//     .lean();
     
-  if (!category) {
-    const error = new Error("카테고리가 존재하지 않습니다.");
-    error.status = 401;
-    return next(error);
-  }
+//   if (!category) {
+//     const error = new Error("카테고리가 존재하지 않습니다.");
+//     error.status = 401;
+//     return next(error);
+//   }
 
-  res.json({
-    error: null,
-    category,
-  });
-});
+//   res.json({
+//     error: null,
+//     category,
+//   });
+// });
 
 module.exports = router;
