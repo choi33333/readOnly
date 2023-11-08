825fab0feb95436d6519ba80bd199b557d26023d
weaall <weaall88@gmail.com>
2023-11-08 16:09:36 +0900

    Fix: product router error codes

--- src/routes/v1/productRouter.js
+++ src/routes/v1/productRouter.js
@@ -46,7 +46,7 @@ router.put("/:id", objectIdValidator, validateError, async (req, res, next) => {
 
   if (typeof(soldAmount) !== "number") {
     const error = new Error("구매수량이 올바르지 않습니다.");
-    error.status = 401;
+    error.status = 400;
     return next(error);
   }
 
@@ -54,7 +54,7 @@ router.put("/:id", objectIdValidator, validateError, async (req, res, next) => {
 
   if (!product) {
     const error = new Error("제품이 존재하지 않습니다.");
-    error.status = 401;
+    error.status = 404;
     return next(error);
   }
 
