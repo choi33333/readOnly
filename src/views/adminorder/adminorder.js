39fd14d28ba49570980dff51a79c17ffdd132a7b
[SW7] 최재혁 <wogur9817@gmail.com>
2023-11-09 17:16:27 +0900

    feat: admin order

--- src/views/adminorder/adminorder.js
+++ src/views/adminorder/adminorder.js
@@ -1,87 +1,51 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const orderNumberInput = document.getElementById("orderNumber");
-    const searchButton = document.getElementById("searchButton");
-    const orderResults = document.getElementById("orderResults");
+const userContainer = document.getElementById('userContainer');
 
-    // 검색 버튼 클릭 이벤트 핸들러
-    searchButton.addEventListener("click", async function () {
-        // 입력된 주문번호 가져오기
-        const orderNumber = orderNumberInput.value;
+window.addEventListener('load', async () => { 
+  const token = localStorage.getItem('Token');
+  if(!isAdmin(token)){
+    location.href = '/notAdmin';
+  }else{
+    try {
+      const fetchResult = await fetchCustom('/api/v1/admin/orders','GET',token);
+      const fetchData = await fetchResult.json();
 
-        try {
-            // API 호출 및 응답 데이터 가져오기
-            const response = await fetch('/api/v1/admin/orders/' + orderNumber, {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": 'Bearer ' + localStorage.getItem('Token'),
-                }
-            });
-
-            // HTTP 상태 코드가 200 OK인 경우
-            if (response.ok) {
-                const orderData = await response.json(); // JSON 응답 데이터 파싱
-
-                // 주문 데이터 화면에 표시
-                displayOrderData(orderData);
-            } else {
-                console.error("주문을 찾지 못했습니다.:", response.status);
-            }
-        } catch (error) {
-            console.error("주문 로드 오류:", error);
-            alert("주문을 불러오는데 오류가 발생했습니다.");
-        }
-    });
-
-    // 주문 데이터를 화면에 표시하는 함수
-    function displayOrderData(orderData) {
-        // 화면을 초기화
-        orderResults.innerHTML = "";
-
-        // 주문 데이터가 있는 경우 테이블에 추가
-        if (orderData) {
-            const tableRow = document.createElement("tr");
-            tableRow.innerHTML = `
-                <td>${orderData.orderNumber}</td>
-                <td>${orderData.productName}</td>
-                <td>${orderData.productPrice}</td>
-                <td>${orderData.quantity}</td>
-                <td>${orderData.orderStatus}</td>
-                <td><button onclick="deleteOrder(${orderData.orderNumber})">삭제</button></td>
-            `;
-            orderResults.appendChild(tableRow);
-        } else {
-            // 주문 데이터가 없는 경우 메시지 표시
-            orderResults.innerHTML = "주문 데이터를 찾을 수 없습니다.";
-        }
+      if(fetchResult.status === 200){
+        console.log('주문 조회 성공');
+        console.log(fetchData.data);
+        orderList(fetchData.data);
+      }else if(fetchResult.status === 403){
+        console.log('권한이 없습니다');
+      }
+    } catch (error) {
+      console.log('err: ', error);
     }
+  }
+})
 
-    // 주문 삭제
-    async function deleteOrder(orderNumber) {
-        try {
-            // 삭제 후 화면에서 해당 주문 데이터 제거
-
-            const deleteResponse = await fetch('/api/v1/admin/orders/' + orderNumber, {
-                method: "DELETE",
-                headers: {
-                    "Authorization": 'Bearer ' + localStorage.getItem('Token'),
-                }
-            });
-
-            if (deleteResponse.ok) {
-                // 주문 삭제 성공했을 때 해당 주문 데이터를 화면에서 제거
-                const tableRowToRemove = document.querySelector(
-                    `#orderResults tr td:first-child:contains("${orderNumber}")`
-                );
-                if (tableRowToRemove) {
-                    tableRowToRemove.parentNode.removeChild(tableRowToRemove);
-                }
-            } else {
-                console.error("주문 삭제 실패:", deleteResponse.status);
-            }
-        } catch (error) {
-            console.error("주문 삭제 오류:", error);
-            alert("주문 삭제에 오류가 발생했습니다.");
-        }
+const orderList = (data) => {
+    userContainer.innerHTML = "";
+    userContainer.innerHTML += `
+      <tr class="userTableHeader">
+        <th class='index'>Index</th>
+        <th class='orderNumber'>주문번호</th>
+        <th class='products'>상품명</th>
+        <th class='quantity'>수량</th>
+        <th class='price'>합계</th>
+        <th class='phoneNumber'>전화번호</th>
+        <th class='orderStatus'>주문처리상태</th>
+      </tr>
+    `
+    for(let i = 0; i < data.length; i++) {
+      userContainer.innerHTML += `
+        <tr id='${i}' class='userTableBody'> 
+          <td class='indexValue'>${i+1}</td>
+          <td class='orderNumberValue'>${data[i].orderNumber}</td>
+          <td class='productsValue'>${data[i].products}</td>
+          <td class='quantityValue'>${data[i].quantity}</td>
+          <td class='priceValue'>${data[i].price}</td>
+          <td class='phoneNumberValue'>${data[i].phoneNumber}</td>
+          <td class='orderStatusValue'>${data[i].orderStatus}</td>
+        </tr>
+      `
     }
-});
+  }
--- src/views/ordercheck/ordercheck.js
+++ src/views/ordercheck/ordercheck.js
@@ -1,14 +1,10 @@
-const orderNumberInput = document.getElementById('orderNumber');
-const orderResultsTableBody = document.getElementById('orderResults');
-
-// 검색 버튼 이벤트 함수
-document.getElementById('searchButton').addEventListener('click', async function (event) {
-    event.preventDefault();
-
-    // 사용자가 입력한 주문 번호 가져오기
-    const orderNumber = orderNumberInput.value;
-
-    // API 및 토큰을 사용하여 주문 정보를 검색
+document.addEventListener('DOMContentLoaded', (event) => {
+    const orderNumberInput = document.getElementById('orderNumber');
+    const orderResultsTableBody = document.getElementById('orderResults');
+    document.getElementById('searchButton').addEventListener('click', async function (event) {
+     event.preventDefault();
+     const orderNumber = orderNumberInput.value;
+    
     try {
         const response = await fetch('/api/v1/orders/me', {
             method: 'GET',
@@ -19,70 +15,54 @@ document.getElementById('searchButton').addEventListener('click', async function
         });
 
         if (response.status === 200) {
-            // 데이터를 res.data를 통해 가져오기
-            const res = await response.json(); // 실제 응답 데이터
-            const orderData = res.data; // 데이터에 res.data를 사용
+            const res = await response.json();
+            const orderData = res.data;
 
-            // 이전 결과를 테이블에서 삭제
             orderResultsTableBody.innerHTML = '';
 
-            // API에서 가져온 데이터 루프
             let orderFound = false;
             for (const order of orderData) {
                 if (order.orderNumber === orderNumber) {
-                    for (const product of order.products) {
-                        const row = document.createElement('tr');
-                        row.innerHTML = `
-                            <td>${order.orderNumber}</td>
-                            <td>${product.productId}</td>
-                            <td>${product.price}</td>
-                            <td>${product.quantity}</td>
-                            <td>${order.orderStatus}</td>
-                            <td><button class="cancel-button" data-order-number="${order.orderNumber}">주문 취소</button></td>
-                        `;
-                        orderResultsTableBody.appendChild(row);
-                    }
                     orderFound = true;
-                    break; // 주문을 찾은 후 종료
+                    for (const product of order.products) {
+                        // 상품 정보 api 호출
+                        const productResponse = await fetch('/api/v1/products/' , {
+                            method: 'GET',
+                            headers: {
+                                "Content-Type": "application/json",
+                                "Authorization": 'Bearer ' + localStorage.getItem('Token'),
+                            }
+                        });
+
+                        if (productResponse.ok) {
+                            const productData = await productResponse.json();
+                            const row = document.createElement('tr');
+                            row.innerHTML = `
+                                <td>${order.orderNumber}</td>
+                                <td>${productData.productId}</td> 
+                                <td>${productData.quantity}</td> 
+                                <td>${order.orderStatus}</td>
+                                <td><button class="cancel-button" data-order-number="${order.orderNumber}">주문 취소</button></td>
+                            `;
+                            orderResultsTableBody.appendChild(row);
+                        } else {
+                            console.error('상품 정보 가져오기 실패: ', productResponse.status);
+                        }
+                    }
+                    break;
                 }
             }
 
-            // 주문을 찾지 못한 경우 메시지 표시
             if (!orderFound) {
                 const noOrderRow = document.createElement('tr');
                 noOrderRow.innerHTML = '<td colspan="6">주문을 찾을 수 없습니다.</td>';
                 orderResultsTableBody.appendChild(noOrderRow);
             }
         } else {
-            console.error('API 요청 실패: ' + response.status);
+            console.error('API 요청 실패: ', response.status);
         }
     } catch (error) {
         console.error('주문 데이터 가져오기 오류: ', error);
     }
+    });
 });
-
-// 주문 취소 함수
-async function cancelOrder(orderNumber) {
-    try {
-        const response = await fetch('/api/v1/orders/' + orderNumber, {
-            method: 'DELETE',
-            headers: {
-                "Authorization": 'Bearer ' + localStorage.getItem('Token'),
-            }
-        });
-
-        if (response.status === 204) {
-            // 주문 취소 성공
-            alert('주문이 성공적으로 취소되었습니다.');
-        } else {
-            // 주문 취소 실패
-            const res = await response.json();
-            const errorMessage = res.message || '주문 취소에 실패했습니다.';
-            alert(errorMessage);
-        }
-        //예외처리
-    } catch (error) {
-        console.error('주문 취소 오류: ', error);
-        alert('주문 취소에 오류가 발생했습니다.');
-    }
-}
