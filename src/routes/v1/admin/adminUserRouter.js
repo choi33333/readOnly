d96454569979ce6208c5ff94a8ab1c9378577a73
soyounRoh <deresa0223@gmail.com>
2023-11-09 17:50:35 +0900

    add : admin controller, service 추가

--- src/routes/v1/admin/adminUserRouter.js
+++ src/routes/v1/admin/adminUserRouter.js
@@ -1,42 +1,12 @@
 const { Router } = require("express");
-const { UserModel } = require("../../../models");
 const { objectIdValidator, validateError } = require("../../../middlewares/validators/index");
+const adminController = require("../../../controllers/adminController");
 const router = Router();
 
 // user 조회
-router.get("/", async (req, res, next) => {
-  const users = await UserModel.find({}).lean();
-
-  if (!users) {
-    const error = new Error("사용자가 없습니다.");
-    error.status = 404;
-    return next(error);
-  }
-
-  res.json({
-    error: null,
-    data: users,
-  });
-});
+router.get("/", adminController.getUser);
 
 // user 삭제
-router.delete("/:id", objectIdValidator, validateError, async (req, res, next) => {
-  const id = req.params.id;
-
-  const deletedUser = await UserModel.deleteOne({ _id: id })
-    .lean();
-
-  if (!deletedUser || deletedUser.length === 0) {
-    const error = new Error("사용자가 존재하지 않습니다.");
-    error.status = 404;
-    return next(error);
-  }
-
-  res.json({
-    error: null,
-    data: deletedUser,
-    message: "사용자가 삭제되었습니다.",
-  });
-});
+router.delete("/:id", objectIdValidator, validateError, adminController.deleteUser);
 
 module.exports = router;
