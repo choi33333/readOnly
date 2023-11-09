6372f2c410bcf1b900ec7f816472123b4d0e6b81
weaall <weaall88@gmail.com>
2023-11-09 19:21:49 +0900

    Fix : upload

--- src/services/adminService.js
+++ src/services/adminService.js
@@ -103,7 +103,7 @@ const adminService = {
 
     const product = await ProductModel.create({
         name: name,
-        category: categoryId._id,
+        category: category,
         categoryName: category,
         author: author,
         price: price,
